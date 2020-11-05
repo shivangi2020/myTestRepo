848fa35f8f10faf9d1a32c5cca50776542128303
vaish <shivangi.vaish.ext@nokia.com>
2020-11-05 22:05:40 +0530

    Initialize application

---
+++ MyProjectCM/MyProjectCM/src/main/java/com/demo/docker/MyProjectCM/MyProjectCmApplication.java
@@ -0,0 +1,37 @@
+package com.demo.docker.MyProjectCM;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@RestController
+public class MyProjectCmApplication {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+		}
+	
+	 @Value("${spring.mail.username}")
+	private String senderUsername;
+	
+	
+	
+	@GetMapping("/helloCM")
+	public String helloCM() {
+		return "Hello World::"+senderUsername;
+	}  
+	
+	
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectCmApplication.class, args);
+	}
+
+}
---
+++ MyProjectCM/MyProjectCM/src/test/java/com/demo/docker/MyProjectCM/MyProjectCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.docker.MyProjectCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
